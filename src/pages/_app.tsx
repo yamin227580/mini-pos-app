f1dbcfa06e98c15e76eafda8f62158ed30d3eae3
Yamin <yaminnyein227580@gmail.com>
2024-01-25 00:03:23 +0630

    fixing for mobile view

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import type { AppProps } from "next/app";
+import "../styles/global.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
